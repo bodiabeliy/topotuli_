00f451635e0005b199173b9ed7a52c897f70ef0c
bodiabeliy <bboda2106@gmail.com>
2025-06-12 14:04:41 +0300

    removing "" in banner component

--- src/components/banners/Banner.tsx
+++ src/components/banners/Banner.tsx
@@ -38,7 +38,7 @@ const Banner = () => {
        <div className="sm:hidden lg:block">
          <div className="bannerTitle pl-0">
           <p className="sm:text-center lg:text-left sm:text-2xl lg:text-5xl text-white font-grotesk  z-[9999]">
-          <span className="text-mainYellow">"Веселі топотулі"</span> - початок успіху <br />для вашої дитини!
+          <span className="text-mainYellow">Веселі топотулі</span> - початок успіху <br />для вашої дитини!
           </p>
           
         </div>
